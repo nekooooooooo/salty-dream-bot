20ad8ceac06ae44c26951bc1ba6a47da4e1ec29b
nekooooooooo <69033860+nekooooooooo@users.noreply.github.com>
2022-11-28 14:24:59 +0800

    Fix logger

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import dotenv
 import discord
 import logging
 from discord.ext import commands
-from modules.logging import get_logger
 
 intents = discord.Intents.default()
 intents.message_content = True
